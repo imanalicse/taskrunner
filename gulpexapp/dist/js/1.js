389a26f56d3e92cf37b2ad1628798663037d1a7e
DESKTOP-1HKAK02\User <imanali.cse@gmail.com>
2018-12-02 21:53:15 +0600

    sass

--- gulpexapp/dist/js/1.js
+++
@@ -1,1 +0,0 @@
-console.log("File 1 file 2"),console.log("file 1 line 2");
--- gulpexapp/gulpfile.js
+++ gulpexapp/gulpfile.js
@@ -1,11 +1,9 @@
 const gulp = require('gulp');
 const uglify = require('gulp-uglify');
 const concat = require('gulp-concat');
-//const sass = require('gulp-sass');
-var uglifycss = require('gulp-uglifycss');
-var watch = require('gulp-watch');
-
-
+const sass = require('gulp-sass');
+const uglifycss = require('gulp-uglifycss');
+const watch = require('gulp-watch');
 
 /**
  * Top level functions
@@ -15,29 +13,32 @@ var watch = require('gulp-watch');
  * gulp.watch - Watch files and folders for changes
  */
 
-gulp.task('js', function(){
-   return gulp.src('src/js/*.js')
-        .pipe(concat('script.js'))  // combine to one file          
-        .pipe(uglify())   // Minify js
-        .pipe(gulp.dest('dist/js'))
- });
+gulp.task('js', function () {
+  return gulp.src('src/js/*.js')
+    .pipe(concat('script.js'))  // combine to one file          
+    .pipe(uglify())   // Minify js
+    .pipe(gulp.dest('dist/js'))
+});
 
- gulp.task('css', function () {
+gulp.task('css', function () {
   gulp.src('src/css/*.css')
     .pipe(uglifycss())
     .pipe(concat('style.css'))
     .pipe(gulp.dest('./dist/css/'));
 });
 
-//  gulp.task('sass', function () {
-//    return gulp.src('src/sass/*.scss')
-//         .pipe(sass().on('error', sass.logError))
-//         .pipe(gulp.dest('dist/css'));
-//  });
+gulp.task('sass', function () {
+  return gulp.src('src/sass/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    //.pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+    //.pipe(concat('style.min.css'))    
+    .pipe(gulp.dest('dist/css'));
+});
 
- gulp.task('default', ['js', 'css']);
+gulp.task('default', ['js', 'css']);
 
- gulp.task('watch', function(){
-      gulp.watch('src/css/*.css', ['js']);
-    gulp.watch('src/css/*.css', ['css']);
- });
+gulp.task('watch', function () {
+  gulp.watch('src/css/*.css', ['js']);
+  gulp.watch('src/css/*.css', ['css']);
+  gulp.watch('src/sass/**/*.scss', ['sass']);
+});
